2c4ac274ff1e6317b61de20c4c619742acc67c71
aitestaitest4 <aitestaitest4@gmail.com>
2024-11-17 09:03:36 +0530

    Delete src directory

--- src/routes/stripeRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {
-  createPaymentIntent,
-} = require("../controllers/stripePaymentController");
-router.route("/create-payment-intent").post(createPaymentIntent);
-
-module.exports = router;
