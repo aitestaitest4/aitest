5865ec272088e9a646b8a68c8c0cf5577595d12c
aitestaitest4 <aitestaitest4@gmail.com>
2024-11-30 11:54:34 +0530

    Delete src directory

--- src/routes/convertTextToAudioRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { uploadAudio } = require("../utils/util");
-const textToAudioController = require("../controllers/textToAudioController");
-
-router.post("/convert-text", textToAudioController.convertTextToAudio);
-
-module.exports = router;
--- src/routes/magicStudioRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const magicstudioAiController = require("../controllers/magicstudioAi");
-
-router.post("/erase-image", magicstudioAiController.magicstudioAiEraseImage);
-
-module.exports = router;
